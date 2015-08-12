4d80fde0dea612a57a8a40f73171626534ee7c4f
Asad Shahabuddin <asad.shahabuddin@gmail.com>
2015-08-11 22:59:42 -0400

    Index creation

--- A7_SpamClassifier/src/main/java/com/ir/global/Properties.java
+++ A7_SpamClassifier/src/main/java/com/ir/global/Properties.java
@@ -11,12 +11,14 @@ import java.util.HashSet;
 public class Properties
 {
     /* Directories and files */
-    public static final String DIR_INPUT     = "E:/Home/ir_data/sc_input";
-    public static final String FILE_STOPLIST = "input/stoplist.txt";
-    public static final String FILE_DOCLIST  = "input/doclist.txt";
+    public static final String DIR_INPUT      = "E:/Home/ir_data/sc_input";
+    public static final String FILE_STOPLIST  = "input/stoplist.txt";
+    public static final String FILE_TRAIN_MAP = "output/train.map";
+    public static final String FILE_TEST_MAP  = "output/test.map";
 
     /* Constants */
     public static final String ES_CLUSTER = "leoscluster";
+    public static final String ES_INDEX   = "a7_dataset";
 
     /* Objects */
     public static HashSet<Character> PUNCTUATIONS = new HashSet<Character>();
--- A7_SpamClassifier/src/main/java/com/ir/sc/Indexer.java
+++ A7_SpamClassifier/src/main/java/com/ir/sc/Indexer.java
@@ -2,10 +2,9 @@ package com.ir.sc;
 
 /* Import list */
 import java.io.*;
-import java.util.List;
 import org.jsoup.Jsoup;
+import java.util.Random;
 import java.util.HashMap;
-import java.util.ArrayList;
 import com.ir.global.Utils;
 import com.ir.global.Properties;
 import org.jsoup.nodes.Document;
@@ -16,7 +15,6 @@ import org.apache.james.mime4j.MimeException;
 import org.apache.james.mime4j.stream.EntityState;
 import org.apache.james.mime4j.stream.MimeTokenStream;
 import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.common.xcontent.XContentBuilder;
 
 /**
  * Author : Asad Shahabuddin
@@ -25,49 +23,122 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 
 public class Indexer
 {
-    private int trainDocCount;
+    private int totalDocCount;
     private int spamDocCount;
     private int hamDocCount;
+    private int trainDocCount;
     private int trainSpamDocCount;
+    private int trainHamDocCount;
+    private int curDocCount;
+    private int curSpamDocCount;
+    private int curHamDocCount;
+    private int curTrainSpamDocCount;
+    private int curTrainHamDocCount;
+
+    private HashMap<String, String> docMap;
+    private HashMap<String, String> trainMap;
+    private HashMap<String, String> testMap;
 
     /**
      * Constructor
      */
     public Indexer()
     {
-        spamDocCount      = 0;
-        hamDocCount       = 0;
-        trainDocCount     = 0;
-        trainSpamDocCount = 0;
+        spamDocCount         = 0;
+        hamDocCount          = 0;
+        trainDocCount        = 0;
+        trainSpamDocCount    = 0;
+        trainHamDocCount     = 0;
+        curDocCount          = 0;
+        curSpamDocCount      = 0;
+        curHamDocCount       = 0;
+        curTrainSpamDocCount = 0;
+        curTrainHamDocCount  = 0;
+
+        docMap   = new HashMap<String, String>();
+        trainMap = new HashMap<String, String>();
+        testMap  = new HashMap<String, String>();
     }
 
     /**
      * Create a map of document names with spam/ham classification.
      * @param file
      *            The input file.
-     * @return
-     *            The map of document names with said classification.
      * @throws IOException
      */
-    public HashMap<String, Boolean> createDocMap(String file)
+    public void createDocMap(String file)
         throws IOException
     {
-        HashMap<String, Boolean> map = new HashMap<String, Boolean>();
         BufferedReader br = new BufferedReader(new FileReader(file));
         String line;
         while((line = br.readLine()) != null)
         {
             if(line.contains("spam"))
             {
-                map.put(line.substring(line.indexOf("inmail")), true);
+                docMap.put(line.substring(line.indexOf("inmail")), "spam");
                 spamDocCount++;
             }
             else
             {
-                map.put(line.substring(line.indexOf("inmail")), false);
+                docMap.put(line.substring(line.indexOf("inmail")), "ham");
                 hamDocCount++;
             }
         }
+    }
+
+    /**
+     * Serialize the maps.
+     * @throws IOException
+     */
+    public void serializeMaps()
+        throws IOException
+    {
+        /* Serialize train map. */
+        FileOutputStream fout = new FileOutputStream(Properties.FILE_TRAIN_MAP);
+        ObjectOutputStream out = new ObjectOutputStream(fout);
+        out.writeObject(trainMap);
+        out.close();
+        fout.close();
+
+        /* Serialize test map. */
+        fout = new FileOutputStream(Properties.FILE_TEST_MAP);
+        out = new ObjectOutputStream(fout);
+        out.writeObject(testMap);
+        out.close();
+        fout.close();
+    }
+
+    /**
+     * Get the map of training documents and their spam classification.
+     * @return
+     *            The aforementioned map.
+     * @throws ClassNotFoundException, IOException
+     */
+    public HashMap<String, String> getTrainMap()
+        throws ClassNotFoundException, IOException
+    {
+        FileInputStream fin = new FileInputStream(Properties.FILE_TRAIN_MAP);
+        ObjectInputStream in = new ObjectInputStream(fin);
+        HashMap<String, String> map = (HashMap<String, String>) in.readObject();
+        in.close();
+        fin.close();
+        return map;
+    }
+
+    /**
+     * Get the map of testing documents and their spam classification.
+     * @return
+     *            The aforementioned map.
+     * @throws ClassNotFoundException, IOException
+     */
+    public HashMap<String, String> getTestMap()
+        throws ClassNotFoundException, IOException
+    {
+        FileInputStream fin = new FileInputStream(Properties.FILE_TEST_MAP);
+        ObjectInputStream in = new ObjectInputStream(fin);
+        HashMap<String, String> map = (HashMap<String, String>) in.readObject();
+        in.close();
+        fin.close();
         return map;
     }
 
@@ -76,9 +147,10 @@ public class Indexer
      */
     public void calcStatistics()
     {
-        int totalDocCount = spamDocCount + hamDocCount;
-        trainDocCount = (totalDocCount * 4) / 5;
-        trainSpamDocCount = (spamDocCount * 4) / 5;
+        totalDocCount     = spamDocCount + hamDocCount;
+        trainDocCount     = (totalDocCount * 4) / 5;
+        trainSpamDocCount = (spamDocCount  * 4) / 5;
+        trainHamDocCount  = (hamDocCount   * 4) / 5;
     }
 
     /**
@@ -87,13 +159,15 @@ public class Indexer
     public void outputStatistics()
     {
         Utils.cout("\n>Statistics\n");
-        Utils.cout("Total      - " + (spamDocCount + hamDocCount) + "\n");
-        Utils.cout("Spam       - " + spamDocCount + "\n");
-        Utils.cout("Ham        - " + hamDocCount + "\n");
-        Utils.cout("Train      - " + trainDocCount + "\n");
-        Utils.cout("Test       - " + (spamDocCount + hamDocCount - trainDocCount) + "\n");
-        Utils.cout("Train spam - " + trainSpamDocCount + "\n");
-        Utils.cout("Test spam  - " + (spamDocCount - trainSpamDocCount) + "\n");
+        Utils.cout("Total                - " + (spamDocCount + hamDocCount) + "\n");
+        Utils.cout("Spam                 - " + spamDocCount + "\n");
+        Utils.cout("Ham                  - " + hamDocCount + "\n");
+        Utils.cout("Train                - " + trainDocCount + "\n");
+        Utils.cout("Test                 - " + (spamDocCount + hamDocCount - trainDocCount) + "\n");
+        Utils.cout("Train spam           - " + trainSpamDocCount + "\n");
+        Utils.cout("Train ham            - " + trainHamDocCount + "\n");
+        Utils.cout("Train spam (current) - " + curTrainSpamDocCount + "\n");
+        Utils.cout("Train ham (current)  - " + curTrainHamDocCount + "\n\n");
     }
 
     /**
@@ -126,19 +200,160 @@ public class Indexer
 
     /**
      * Convert formatted text to plain text.
-     * @param content
-     *            The formatted text.
+     * @param file
+     *            The input file.
      * @return
      *            The plain text.
      * @throws IOException
      */
-    public String plainText(String content)
+    public String plainText(File file)
         throws IOException
     {
-        Document document = Jsoup.parse(content);
+        /* Read the contents to file into memory. */
+        FileInputStream finStream = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        finStream.read(data);
+        finStream.close();
+
+        /* Convert formatted text to plain text. */
+        Document document = Jsoup.parse(new String(data));
         String s = document.body().text();
         return s.substring(s.indexOf("][ ") + 3)
                 .replaceAll(" ]]", "");
+    }
+
+    /**
+     * Generate random 0s and 1s.
+     * @return
+     *           0 or 1, chosen randomly.
+     */
+    public int random()
+    {
+        return new Random().nextInt(2);
+    }
+
+    /**
+     * Partition spam documents into train and test data.
+     * @param file
+     *            The input file.
+     * @return
+     *            Train/test classification.
+     */
+    public String partitionSpam(String file)
+    {
+        int key = 0;
+        String val;
+        /* Determine partition. */
+        if(trainSpamDocCount - curTrainSpamDocCount < spamDocCount - curSpamDocCount)
+        {
+            key = (curTrainSpamDocCount == trainSpamDocCount) ? 1 : random();
+        }
+
+        /* Update the state based on partition. */
+        if(key == 0)
+        {
+            val = "train";
+            curTrainSpamDocCount++;
+            trainMap.put(file, "spam");
+        }
+        else
+        {
+            val = "test";
+            testMap.put(file, "spam");
+        }
+        return val;
+    }
+
+    /**
+     * Partition ham documents into train and test data.
+     * @param file
+     *            The input file.
+     * @return
+     *            Train/test classification.
+     */
+    public String partitionHam(String file)
+    {
+        int key = 0;
+        String val;
+        /* Determine partition. */
+        if(trainHamDocCount - curTrainHamDocCount < hamDocCount - curHamDocCount)
+        {
+            key = (curTrainHamDocCount == trainHamDocCount) ? 1 : random();
+        }
+
+        /* Update the state based on partition. */
+        if(key == 0)
+        {
+            val = "train";
+            curTrainHamDocCount++;
+            trainMap.put(file, "ham");
+        }
+        else
+        {
+            val = "test";
+            testMap.put(file, "ham");
+        }
+        return val;
+    }
+
+    /**
+     * Index a file.
+     * @param client
+     *            The Elasticsearch client.
+     * @param file
+     *            The file.
+     * @throws IOException
+     */
+    public void indexFile(Client client, File file)
+        throws IOException
+    {
+        String split;
+        if(docMap.get(file.getName()).equals("spam"))
+        {
+            split = partitionSpam(file.getName());
+            curSpamDocCount++;
+        }
+        else
+        {
+            split = partitionHam(file.getName());
+            curHamDocCount++;
+        }
+
+        /* Update the index. */
+        client.prepareIndex(Properties.ES_INDEX, "document", "" + file.getName())
+              .setSource(XContentFactory.jsonBuilder()
+                                        .startObject()
+                                        .field("file_name", file.getName())
+                                        .field("label"    , docMap.get(file.getName()))
+                                        .field("body"     , plainText(file))
+                                        .field("split"    , split)
+                                        .endObject())
+              .execute()
+              .actionGet();
+    }
+
+    /**
+     * Index a document corpus.
+     * @param client
+     *            The Elasticsearch client.
+     * @throws IOException
+     */
+    public void index(Client client)
+        throws IOException
+    {
+        File[] files = new File(Properties.DIR_INPUT + "/data").listFiles();
+        for(File file : files)
+        {
+            /* Index individual documents. */
+            indexFile(client, file);
+            if(++curDocCount % 100 == 0)
+            {
+                Utils.echo("Processed the data for " + curDocCount + " documents");
+            }
+        }
+        /* Serialize maps and output statistics. */
+        serializeMaps();
+        outputStatistics();
     }
 
     /**
@@ -147,23 +362,37 @@ public class Indexer
      *            Program arguments.
      */
     public static void main(String[] args)
-        throws IOException
     {
         /* Calculate start time. */
         long startTime = System.nanoTime();
-        Utils.cout("\n>Indexer\n");
+        Utils.cout("\n===========");
+        Utils.cout("\n| INDEXER |");
+        Utils.cout("\n===========");
+        Utils.cout("\n");
         Utils.echo("Cluster name   - " + Properties.ES_CLUSTER);
         Utils.echo("Data directory - " + Properties.DIR_INPUT);
 
-        Node node = NodeBuilder.nodeBuilder()
-                               .client(true)
-                               .clusterName(Properties.ES_CLUSTER)
-                               .node();
-        Client client = node.client();
-        File[] files = new File(Properties.DIR_INPUT).listFiles();
-        for(File file : files)
+        Node node = null;
+        try
         {
-            // TODO
+            Indexer i = new Indexer();
+            node = NodeBuilder.nodeBuilder().client(true).clusterName(Properties.ES_CLUSTER).node();
+            Client client = node.client();
+            i.createDocMap(Properties.DIR_INPUT + "/full/index");
+            i.calcStatistics();
+            i.index(client);
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+        finally
+        {
+            if(node != null)
+            {
+                node.close();
+            }
+            Utils.elapsedTime(startTime, "\nCreation of index completed");
         }
     }
 }
